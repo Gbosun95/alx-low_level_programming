f31f21d6326fe54755259fa24b943f6cb4e01150
Gbosun95 <Gbosun@yahoo.com>
2022-03-21 22:15:10 -0700

    load

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,13 @@
 void reset_to_98(int *n);
+
 int _strlen(char *s);
+
 void swap_int(int *a, int *b);
+
 void _puts(char *str);
+
 void print_rev(char *s);
+
 void rev_string(char *s);
+
+void puts2(char *str);
